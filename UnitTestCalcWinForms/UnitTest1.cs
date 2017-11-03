2e6bfa4f163993a798f212bdfaec46df7c3ce5ce
ITHope <nadya.berdnik@mail.ru>
2017-11-03 13:50:14 +0200

    Add NUnit

--- UnitTestCalcWinForms/UnitTest1.cs
+++ UnitTestCalcWinForms/UnitTest1.cs
@@ -1,6 +1,28 @@
 ﻿using System;
+using NUnit.Framework;
 
-namespace UnitTestCalcWinForms
+namespace NUnitTestCalcWinForms
 {
-
+    [TestFixture]
+    public class UnitTestCalc
+    {
+        
+        [TestCase(0, 0, 0, '+')]
+        [TestCase(4, 5, 9, '+')]
+        [TestCase(78, 2, 80, '+')]
+        [TestCase(0, 0, 0, '-')]
+        [TestCase(4, 5, -1, '-')]
+        [TestCase(78, 2, 76, '-')]
+        [TestCase(1, 1, 1, '*')]
+        [TestCase(4, 5, 20, '*')]
+        [TestCase(78, 2, 156, '*')]
+        [TestCase(1, 1, 1, '/')]
+        [TestCase(20, 5, 4, '/')]
+        [TestCase(78, 2, 39, '/')]
+        public void TestCalc(int a, int b, int exp, char op)
+        {
+            int res = CalcWinFormsBtns.Calculator.Calc(a, b, op);
+            Assert.AreEqual(exp, res);
+        }
+    }
 }
